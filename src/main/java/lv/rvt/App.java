7d5820f43ec2764b9ff63db821b2a0185b3b0af7
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-09-13 17:42:46 +0000

    Part 1-2; Ada Lovelace

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,14 @@
 package lv.rvt;
 
+
+
 public class App 
 {
     public static void main( String[] args )
     {
         // Make your Hello World programm!
-        System.out.println("RVT");
+        System.out.println("Ada Lovelace");
+
+        
     }
 }
