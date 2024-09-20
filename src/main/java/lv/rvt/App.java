577d819ac64c136ed765f48ae7b035525d794276
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-09-20 10:30:02 +0000

    simple calkulator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,14 +1,29 @@
 package lv.rvt;
 
-
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
-        System.out.println("Ada Lovelace");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Frist number: ");
+        int firstNum = Integer.valueOf(scanner.nextLine());
         
+        System.out.println("Second number: ");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+
+        int sum = firstNum + secondNum;
+        int minus = firstNum - secondNum;
+        int reiz = firstNum * secondNum;
+        double dal = firstNum / secondNum;
+        
+        System.out.println(firstNum + "+" + secondNum + "=" + sum);
+        System.out.println(firstNum + "-" + secondNum + "=" + minus);
+        System.out.println(firstNum + "*" + secondNum + "=" + reiz);
+        System.out.println(firstNum + "/" + secondNum + "=" + dal);
+
+
     }
 }
