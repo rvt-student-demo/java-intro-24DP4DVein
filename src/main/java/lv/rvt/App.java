7c76b829e1542c0a5fc5e0bf81339cbb380e1f35
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-09-27 10:43:54 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,14 +8,25 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Speed: ");
+        System.out.println("Mark: ");
         int firstNum = Integer.valueOf(scanner.nextLine());
-        
-        if (firstNum > 120) {
-            System.out.println("Speeding ticket!");
+             
+        if (firstNum == 0) {
+            System.out.println("imsposible!");
+        } else if (firstNum <= 49) {
+            System.out.println("fail");
+        } else if (firstNum <= 59){
+            System.out.println("1");
+        } else if (firstNum <= 69){
+            System.out.println("2");
+        } else if (firstNum <= 79){
+            System.out.println("3");
+        } else if (firstNum <= 89){
+            System.out.println("3");
+        } else if (firstNum <= 99){
+            System.out.println("4");
+        } else {
+            System.out.println("incredible!");
         }
-        
-
-
     }
 }
