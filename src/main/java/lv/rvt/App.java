63ce819b9b3f7b606a144573ce741c7867073bfb
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-10-10 11:12:02 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,24 +6,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int sk = 0;
+        while(true){
 
-        System.out.println("Gift: ");
-        int firstNum = Integer.valueOf(scanner.nextLine());
-             
-        if (firstNum < 5000){
-            System.out.println("Nothing");
-        } else if (firstNum < 25000){
-            System.out.println(100+(firstNum-5000)*0.08);
-        } else if (firstNum < 55000){
-            System.out.println(1700+(firstNum-25000)*0.10);
-        } else if (firstNum < 200000){
-            System.out.println(4700+(firstNum-55000)*0.12);
-        } else if (firstNum < 1000000){
-            System.out.println(22000+(firstNum-55000)*0.15);    
-        } else {
-            System.out.println(142100+(firstNum-1000000)*0.17);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Give a number: ");
+
+            int Num = Integer.valueOf(scanner.nextLine());
+
+            if (Num == 0) {
+                System.out.println("Number of numbers:"+ sk);
+                System.out.println("Sum of the numbers:"+ sum);
+                break;
+            } else {
+                sk = sk + 1;
+                sum = sum + Num;
+            }
         }
-
     }
 }
