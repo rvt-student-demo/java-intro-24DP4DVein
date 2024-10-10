3526124fd5deeb4b55fe53714d963d017a9aef93
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-10-10 12:01:20 +0000

    no n lidz 100

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,23 +6,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        int sum = 0;
-        int sk = 0;
-        while(true){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number: ");
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Give a number: ");
-
-            int Num = Integer.valueOf(scanner.nextLine());
-
-            if (Num == 0) {
-                System.out.println("Number of numbers:"+ sk);
-                System.out.println("Sum of the numbers:"+ sum);
-                break;
-            } else {
-                sk = sk + 1;
-                sum = sum + Num;
-            }
+        for ( int Num = Integer.valueOf(scanner.nextLine()); Num < 101; Num++) {
+            System.out.println(Num);
         }
     }
 }
+
