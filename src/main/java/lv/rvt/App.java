2059619235f94b8e1b0e39b9be4ffedf96ac57c7
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-10-31 13:22:10 +0000

    Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,11 +5,30 @@ public class App
     public class Main {
         public static void main(String[] args) {
           
-            int[] val = {0, 1, 2, 3};
+            int[] mas = new int[3];
 
-            int sum = val[0] + val[1] + val[2] + val[3];
+            mas[0] = 10;
+            mas[1] = 113;
+            mas[2] = 123;
 
-            System.out.println("Sum of number = " + sum);
+            int sum = 0;
+            for (int i = 0; i < mas.length; i++){
+                System.out.println(mas[i]);
+                sum = sum + mas[i];
+            }
+            System.out.println("Sum: " + sum);
+
+            int[] val = {13, -4, 82, 17};
+            int[] twice = new int[4];
+
+            System.out.println();
+            System.out.println();
+            System.out.println("Orig. mas.: " + " " + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
+
+            for (int i = 0; i < val.length; i++){
+                twice[i] = val[i] * 2;
+            }
+            System.out.println("Second mas. : " + " " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3]);
 
         }
     }
