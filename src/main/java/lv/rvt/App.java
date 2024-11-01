29aad4f18f1261600ba3538cb5ce65f8e3983c12
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-11-01 10:43:51 +0000

    Same Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,11 +11,30 @@ public class App
 
             
 
-            for (int i = 0; i < valA.length; i++){
+            for (int i = 0; i < valA.length; i++)
+            {
                 sum[i] = valA[i] + valB[i];
             }
             
             System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+
+            // Same Sum
+
+            int[] valC   = { 13, -22,  82,  17}; 
+            int[] valD   = {  0,   0,   0,   0}; 
+            int[] atb = new int [4];
+
+            for (int i = 0;i<valC.length;i++)
+            {
+                valD[i] = 25 - valC[i];
+                atb[i] = valC[i] + valD[i];
+                System.out.println(valC[i] + "+" + valD[i] + "=" + atb[i]);
+                
+
+            
+            }
+
+            //
         }
     }
 }
