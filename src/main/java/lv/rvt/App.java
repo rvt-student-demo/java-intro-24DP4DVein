3eaafa7bb3b118a5fa8e128202c681409a85e98b
24DP4DVein <166484832+24DP4DVein@users.noreply.github.com>
2024-11-01 10:13:54 +0000

    Three Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,32 +4,18 @@ public class App
 {
     public class Main {
         public static void main(String[] args) {
-          
-            int[] mas = new int[3];
+            // Three Arrays
+            int[] valA   = { 13, -22,  82,  17}; 
+            int[] valB   = {-12,  24, -79, -13};
+            int[] sum    = {  0,   0,   0,   0};
 
-            mas[0] = 10;
-            mas[1] = 113;
-            mas[2] = 123;
+            
 
-            int sum = 0;
-            for (int i = 0; i < mas.length; i++){
-                System.out.println(mas[i]);
-                sum = sum + mas[i];
+            for (int i = 0; i < valA.length; i++){
+                sum[i] = valA[i] + valB[i];
             }
-            System.out.println("Sum: " + sum);
-
-            int[] val = {13, -4, 82, 17};
-            int[] twice = new int[4];
-
-            System.out.println();
-            System.out.println();
-            System.out.println("Orig. mas.: " + " " + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
-
-            for (int i = 0; i < val.length; i++){
-                twice[i] = val[i] * 2;
-            }
-            System.out.println("Second mas. : " + " " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3]);
-
+            
+            System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
         }
     }
 }
