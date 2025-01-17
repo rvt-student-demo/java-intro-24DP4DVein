4695163208aef564b21443e374a02d0652e76774
Daniels Veinbergs <166484832+24DP4DVein@users.noreply.github.com>
2025-01-17 11:12:49 +0000

    box

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,29 +1,18 @@
 package lv.rvt;
 
+import javax.swing.Box;
+
 public class App {
-        public class Main {
-            public static void main(String[] args) {
-
-                
-                HealthStation childrensHospital = new HealthStation();
-
-                Person ethan = new Person("Ethan", 1, 110, 7);
-                Person peter = new Person("Peter", 33, 176, 85);
-
-                System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-                System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-
-                childrensHospital.feed(ethan);
-                childrensHospital.feed(ethan);
-                childrensHospital.feed(ethan);
-
-                System.out.println("");
-
-                System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-                System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-
-            }
-        }
+    class BoxTester
+    {
+    
+      public static void main ( String[] args )
+      {
+         Box box = new  Box(2.5, 5.0, 6.0);
+    
+         System.out.println( "Area: "  + box.area() + " volume: " + box. volume() );
+    
+    
+      }
     }
-
 
